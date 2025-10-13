cf361d59debb4d179aa7007484dd5bc2b728718e
Luis Gallegos <143466516+kyg0ff@users.noreply.github.com>
2025-10-13 00:20:04 -0500

    Delete backend/script.js

--- backend/script.js
+++
@@ -1,4 +0,0 @@
-// script.js (Raíz del proyecto)
-// Puedes usar este archivo para lógica de servidor (si tienes un backend en Node.js)
-// Para la lógica de la interfaz, usa un archivo como 'app.js' dentro de /interfaces/
-console.log("Servidor configurado. Ejecuta 'npm start' para iniciar live-server.");
