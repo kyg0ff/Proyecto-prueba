c66e7aa37265e0103713dc4f68ce16acd7d6c20c
LuisDevUbuntu <lufegaa16@gmail.com>
2025-10-13 00:07:12 -0500

    Añado interfaces de pagina principal y perfiles

---
+++ backend/script.js
@@ -0,0 +1,5 @@
+// script.js (En la raíz del proyecto)
+// Este archivo está aquí solo por convención del nombre
+// La lógica principal del cliente debe ir en /interfaces/app.js
+
+console.log("El entorno de Node.js ha cargado. Usa 'npm start' para ver la interfaz.");
